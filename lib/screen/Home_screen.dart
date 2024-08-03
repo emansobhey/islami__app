7ee64a93f3aaf25b97f4291a63b1b9a8977c16c3
unknown <emansobhey8@gmail.com>
2024-08-03 04:35:25 +0300

    first commit

---
+++ lib/screen/Home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScrean extends StatelessWidget {
+  const HomeScrean({super.key});
+  static const String id = 'Home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text('fff'),),
+    );
+  }
+}
